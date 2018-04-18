7554c732ca12cd539eedeacfe97bac9d53102dde
diksha <shreediksha99@gmail.com>
2018-04-18 15:45:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, render_template
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return render_template('resume.html')
+ 
+if __name__ == '__main__':
+    app.run()
